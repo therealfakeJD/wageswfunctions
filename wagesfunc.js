490dee8da4cdeb19c45a213912754792a13534d5
jdbogieman <justin.diblasi@gmail.com>
2016-01-05 12:22:36 -0600

    wage problem

---
+++ wagesfunc.js
@@ -0,0 +1,44 @@
+"use strict"
+
+/*var hours = window.prompt("Hours Worked");
+var rate = window.prompt("Hourly Pay");
+var specot1 = (hours - 40)
+var specot2 = (rate * 1.5)
+var weekhours = 40
+var shortw;
+shortw = sweek(hours,rate);
+var regular;
+regular = weekpay(weekhours , rate);
+var special;
+special = otPay(specot1, specot2)
+
+if (hours > 40){
+	console.log("weekpay","$",regular);
+	console.log("overtime","$",special);
+	}
+if (hours <= 40){
+	console.log("shortweek","$",shortw);
+}
+	
+function sweek(hours,rate){
+	var shortweek;
+	shortweek = hours * rate;
+	return shortweek
+}
+
+function weekpay(weekhours, rate){
+	var weekpay;
+	weekpay = weekhours * rate;
+	return weekpay;
+}
+
+function otPay(specot1, specot2){
+	var overtime;
+	overtime = (specot1* specot2);
+	return overtime;
+		
+}*/
+
+function myName(Justin, DiBlasi){
+	console.log("Justin DiBlasi")
+}
